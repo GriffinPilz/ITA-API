be805bf58c77a6c4874942ab5c0716b6c43c26b5
Griffin Pilz <griffinpilz@Griffins-Mac-mini.local>
2017-10-09 10:51:01 -0400

    initial commit

---
+++ api/handlers/atlantic.js
@@ -0,0 +1,125 @@
+var mysql  = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'thereisnone',
+  database : 'ITD',
+  port     : '3306'
+});
+
+module.exports.dbHello = {
+  handler: function (request, reply) {
+
+    return reply({ result: 'DB Hello hapi!' });
+  }
+};
+
+module.exports.issuesList = {
+
+  handler: function (request, reply) {
+
+    var sql = "SELECT * FROM ITD.final_itd;";
+
+    connection.query(sql, [], function(err, rows, fields) {
+
+      //Error Response
+      if (err) throw err;
+
+      //Invalid Response
+      if(rows.length == 0)
+      {
+        return reply({"status":"Invalid"});
+      }
+
+      //Valid Login
+      return reply(rows);
+
+    });
+  }
+
+  
+};
+
+module.exports.customerUpdate = {
+  
+  handler: function (request, reply) {
+  
+    let sql = "";
+    let paramArray = [];
+
+    if(request.payload.RowID == 0 )
+    {
+      sql = `insert into idp_custom_cart (Customer, CustomerURLParam, Image, WebURL, Active, password, BillToAddress,StdShipping, ExpShipping, M2MID, POTracking, creditCard, allowPO) 
+        values (?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+
+      paramArray = [
+        request.payload.Customer, 
+        request.payload.CustomerURLParam, 
+        request.payload.Image, 
+        request.payload.WebURL, 
+        request.payload.Active, 
+        request.payload.password, 
+        request.payload.BillToAddress, 
+        request.payload.StdShipping, 
+        request.payload.ExpShipping, 
+        request.payload.M2MID, 
+        request.payload.POTracking, 
+        request.payload.creditCard, 
+        request.payload.allowPO 
+      ];
+    }
+    else
+    {
+      sql = `update idp_custom_cart
+        set Customer = ?,
+            CustomerURLParam = ?,
+            Image = ?,
+            WebURL = ?,
+            Active = ?,
+            password = ?,
+            BillToAddress = ?,
+            StdShipping = ?,
+            ExpShipping = ?,
+            M2MID = ?,
+            POTracking = ?,
+            creditCard = ?,
+            allowPO = ?
+        where RowID = ?`
+
+      paramArray = [
+        request.payload.Customer, 
+        request.payload.CustomerURLParam, 
+        request.payload.Image, 
+        request.payload.WebURL, 
+        request.payload.Active, 
+        request.payload.password, 
+        request.payload.BillToAddress, 
+        request.payload.StdShipping, 
+        request.payload.ExpShipping, 
+        request.payload.M2MID, 
+        request.payload.POTracking, 
+        request.payload.creditCard, 
+        request.payload.allowPO,
+        request.payload.RowID
+      ];
+
+    }
+      
+    connection.query(sql, paramArray, function(err, rows, fields) {
+  
+      //Error Response
+      if (err) throw err;
+  
+      //Invalid Response
+      if(rows.length == 0)
+      {
+        return reply({"status":"Invalid"});
+      }
+  
+      //Valid 
+      return reply({"status":"valid"});
+  
+    });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const Home = require('./handlers/home');
+const atlantic = require('./handlers/atlantic');
 
 exports.register = (plugin, options, next) => {
 
   plugin.route([
     { method: 'GET', path: '/', config: Home.hello },
     { method: 'GET', path: '/restricted', config: Home.restricted },
-    { method: 'GET', path: '/{path*}', config: Home.notFound }
+    { method: 'GET', path: '/{path*}', config: Home.notFound },
+    { method: 'GET', path: '/atlantic', config: atlantic.dbHello },
+    { method: 'GET', path: '/atlantic/issues', config: atlantic.issuesList }
   ]);
 
   next();
@@ -14,3 +17,4 @@ exports.register = (plugin, options, next) => {
 exports.register.attributes = {
   name: 'api'
 };
+
