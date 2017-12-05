73f7560f38d93104a951f26490429fc78e7523d7
Griffin Pilz <griffinpilz@Griffins-Mac-mini.local>
2017-12-05 16:34:10 -0500

    company change

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const Home = require('./handlers/home');
-const atlantic = require('./handlers/atlantic');
+const company = require('./handlers/company');
 
 exports.register = (plugin, options, next) => {
 
@@ -8,21 +8,21 @@ exports.register = (plugin, options, next) => {
     { method: 'GET', path: '/', config: Home.hello },
     { method: 'GET', path: '/restricted', config: Home.restricted },
     { method: 'GET', path: '/{path*}', config: Home.notFound },
-    { method: 'GET', path: '/atlantic', config: atlantic.dbHello },
-    { method: 'GET', path: '/atlantic/issues', config: atlantic.issuesList },
-    { method: 'GET', path: '/atlantic/customers', config: atlantic.customers },
-    { method: 'GET', path: '/atlantic/employees', config: atlantic.employees },
-    { method: 'POST', path: '/atlantic/issuesHistoryget', config: atlantic.issuesHistoryget },
+    { method: 'GET', path: '/company', config: company.dbHello },
+    { method: 'GET', path: '/company/issues', config: company.issuesList },
+    { method: 'GET', path: '/company/customers', config: company.customers },
+    { method: 'GET', path: '/company/employees', config: company.employees },
+    { method: 'POST', path: '/company/issuesHistoryget', config: company.issuesHistoryget },
 
     //POST
-    { method: 'POST', path: '/atlantic/auth', config: atlantic.customerAuthCheck },
-    { method: 'POST', path: '/atlantic/customerUpdate', config: atlantic.customerUpdate },
-    { method: 'POST', path: '/atlantic/ticketInsert', config: atlantic.ticketInsert },
-    { method: 'POST', path: '/atlantic/customerModalUpdate', config: atlantic.customerModalUpdate },
-    { method: 'POST', path: '/atlantic/employeeModalUpdate', config: atlantic.employeeModalUpdate },
-    { method: 'POST', path: '/atlantic/issueModalUpdate', config: atlantic.issueModalUpdate },
-    { method: 'POST', path: '/atlantic/issueHistoryModalUpdate', config: atlantic.issueHistoryModalUpdate },
-    { method: 'POST', path: '/atlantic/dayIssues', config: atlantic.dayIssues }
+    { method: 'POST', path: '/company/auth', config: company.customerAuthCheck },
+    { method: 'POST', path: '/company/customerUpdate', config: company.customerUpdate },
+    { method: 'POST', path: '/company/ticketInsert', config: company.ticketInsert },
+    { method: 'POST', path: '/company/customerModalUpdate', config: company.customerModalUpdate },
+    { method: 'POST', path: '/company/employeeModalUpdate', config: company.employeeModalUpdate },
+    { method: 'POST', path: '/company/issueModalUpdate', config: company.issueModalUpdate },
+    { method: 'POST', path: '/company/issueHistoryModalUpdate', config: company.issueHistoryModalUpdate },
+    { method: 'POST', path: '/company/dayIssues', config: company.dayIssues }
   ]);
   
   next();
